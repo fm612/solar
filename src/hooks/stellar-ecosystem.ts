ae11326c83ecd297b91ca890e54e5c9a2a7532c4
Marcel Ebert <mail@marcel-ebert.de>
2020-01-13 16:48:58 +0100

    Add 'usePriceConversion' hook

--- src/hooks/stellar-ecosystem.ts
+++ src/hooks/stellar-ecosystem.ts
@@ -2,6 +2,7 @@ import React from "react"
 import { trackError } from "../context/notifications"
 import { AccountRecord, fetchWellknownAccounts } from "../lib/stellar-expert"
 import { AssetRecord, fetchAllAssets } from "../lib/stellar-ticker"
+import { CurrencyCode, fetchCryptoPrice } from "../lib/currency-conversion"
 import { tickerAssetsCache, wellKnownAccountsCache } from "./_caches"
 import { useForceRerender } from "./util"
 
@@ -41,4 +42,29 @@ export function useWellKnownAccounts(testnet: boolean) {
   }, [accounts])
 
   return wellknownAccounts
+}
+
+export function usePriceConversion(currency: CurrencyCode, testnet: boolean) {
+  const [price, setPrice] = React.useState<number>(0)
+
+  React.useEffect(() => {
+    fetchCryptoPrice(currency, testnet).then(setPrice)
+
+    const interval = setInterval(() => {
+      fetchCryptoPrice(currency, testnet).then(setPrice)
+    }, 60 * 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  const conversion = React.useMemo(() => {
+    return {
+      price,
+      convertXLM(amount: number): number {
+        return price * amount
+      }
+    }
+  }, [price])
+
+  return conversion
 }
