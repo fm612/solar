1948ecfc90674bec17f8a76c8819d170b7a5c6f6
Marcel Ebert <mail@marcel-ebert.de>
2020-02-03 15:32:57 -0300

    Replace hard-coded strings for multi-sig service

--- src/components/TransactionSender.tsx
+++ src/components/TransactionSender.tsx
@@ -231,8 +231,8 @@ class TransactionSender extends React.Component<Props, State> {
 
     try {
       const promise = this.state.signatureRequest
-        ? collateSignature(this.state.signatureRequest, signedTransaction)
-        : submitNewSignatureRequest(this.props.settings.multiSignatureServiceURL, signatureRequestURI)
+        ? collateSignature(this.state.signatureRequest, signedTransaction, this.props.t)
+        : submitNewSignatureRequest(this.props.settings.multiSignatureServiceURL, signatureRequestURI, this.props.t)
 
       this.setSubmissionPromise(promise)
       this.setState({ submissionType: SubmissionType.multisig })
